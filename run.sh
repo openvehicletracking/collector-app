f6edd1f73f1642c595c31bbf52553f2778bc4849
Yunus Oksuz <yunusoksuz@gmail.com>
2017-07-04 22:42:55 +0300

    core, xtakip version

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ln -s /opt/app/config/nginx-$APP_ENV.conf /etc/nginx/sites-enabled/openmts.$APP_ENV.conf && \
 service nginx start && \
-/usr/bin/java \-Xmx756m -Xms256m \
+/usr/bin/java -Xmx1g -Xms256m \
 -Dconf=/opt/app/config/config-$APP_ENV.json \
 -Dfile.encoding=UTF-8 \
 -jar /opt/app/motodev-collector.jar
