e99db56cf97637ac56499c35c805a0d4b1a30a68
Yunus Oksuz <yunusoksuz@gmail.com>
2017-11-12 10:21:32 +0300

    distributed deviceStateCache

--- src/main/java/com/openvehicletracking/collector/cache/DeviceStateCache.java
+++ src/main/java/com/openvehicletracking/collector/cache/DeviceStateCache.java
@@ -1,11 +1,11 @@
 package com.openvehicletracking.collector.cache;
 
-import com.google.gson.JsonParser;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import com.openvehicletracking.core.DeviceState;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
-
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by oksuz on 28/09/2017.
@@ -13,11 +13,15 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class DeviceStateCache {
 
-    private static final ConcurrentHashMap<String, DeviceState> state = new ConcurrentHashMap<>();
+    private static final String SHARED_MAP_NAME = "mts.devicestate";
+
+    private final IMap<String, DeviceState> state;
     private static final DeviceStateCache INSTANCE = new DeviceStateCache();
 
-    private DeviceStateCache() {}
-
+    private DeviceStateCache() {
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(new ClientConfig().setGroupConfig(new GroupConfig("dev", "dev-pass")));
+        state = client.getMap(SHARED_MAP_NAME);
+    }
 
     public static DeviceStateCache getInstance() {
         return INSTANCE;
@@ -31,21 +35,5 @@ public class DeviceStateCache {
 
     public DeviceState get(String deviceId) {
         return state.get(deviceId);
-    }
-
-    public JsonArray getAllAsJson() {
-        JsonArray data = new JsonArray();
-        state.forEach((deviceId, state) -> data.add(new JsonObject(state.toJson().toString())));
-        return data;
-    }
-
-    public void load(JsonArray data) {
-        state.clear();
-        JsonParser parser = new JsonParser();
-        for (int i = 0; i < data.size(); i++) {
-            JsonObject deviceStateJson = data.getJsonObject(i);
-            DeviceState deviceState = DeviceState.fromJson(parser.parse(deviceStateJson.toString()).getAsJsonObject());
-            put(deviceState);
-        }
     }
 }
